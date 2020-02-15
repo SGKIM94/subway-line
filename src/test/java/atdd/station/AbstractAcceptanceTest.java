2f6f9dffee8a1c530bc4649d21c1575c61ee03a8
sanggu <tkdrn8578@naver.com>
2020-02-16 01:43:02 +0900

    [ADD] Acceptance Test 에 대한 설정 중복이 발생함에 따른 Abstract 설정 클래스 추가

---
+++ src/test/java/atdd/station/AbstractAcceptanceTest.java
@@ -0,0 +1,18 @@
+package atdd.station;
+
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient(timeout = "100000")
+@ExtendWith(SoftAssertionsExtension.class)
+public class AbstractAcceptanceTest {
+    @Autowired
+    protected WebTestClient webTestClient;
+}
