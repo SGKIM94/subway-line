7afdcdf888677c2527d748530b2f2193b3086562
sanggu <tkdrn8578@naver.com>
2020-02-23 21:39:21 +0900

    [ADD] SoftAssertionExtension 의 설정을 가지고 있는 추상 클래스추가

---
+++ src/test/java/atdd/station/SoftAssertionTest.java
@@ -0,0 +1,8 @@
+package atdd.station;
+
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SoftAssertionsExtension.class)
+public class SoftAssertionTest {
+}
