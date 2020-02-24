ca3b7376b5c62fe46d71cd7959d73d03f5513b70
sanggu <tkdrn8578@naver.com>
2020-02-24 22:08:59 +0900

    [ADD] dirtiesContext 를 제거하고 database 를 비움으로써 테스트 격리하도록 수정
     * dirtiesContext 를 사용하면, 테스트 격리도 제대로 이뤄지지 않고 ApplicationContext 도 매번 새로 띄우기 때문에
     시간이 많이 발생함

--- src/test/java/atdd/station/AbstractAcceptanceTest.java
+++ src/test/java/atdd/station/AbstractAcceptanceTest.java
@@ -1,18 +1,27 @@
-package atdd.station;
+package atdd.path;
 
-import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
-import org.junit.jupiter.api.extension.ExtendWith;
+import atdd.station.SoftAssertionTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient(timeout = "100000")
-@ExtendWith(SoftAssertionsExtension.class)
-public class AbstractAcceptanceTest {
+public class AbstractAcceptanceTest extends SoftAssertionTest {
     @Autowired
-    protected WebTestClient webTestClient;
+    public WebTestClient webTestClient;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    protected void cleanAllDatabases() {
+        JdbcTestUtils.deleteFromTables(jdbcTemplate, "STATION", "LINE", "EDGE", "USER");
+        jdbcTemplate.update("ALTER TABLE STATION ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE LINE ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE EDGE ALTER COLUMN id RESTART WITH 1");
+        jdbcTemplate.update("ALTER TABLE USER ALTER COLUMN id RESTART WITH 1");
+    }
 }
