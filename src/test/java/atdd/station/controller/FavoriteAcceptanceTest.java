180a7afe70e91b43f4f65674a616a11e1b39d4bc
sanggu <tkdrn8578@naver.com>
2020-02-25 23:05:01 +0900

    [ADD] Favorite 에 대한 AcceptanceTest 추가

---
+++ src/test/java/atdd/station/controller/FavoriteAcceptanceTest.java
@@ -0,0 +1,53 @@
+package atdd.station.controller;
+
+import atdd.path.domain.Favorite;
+import atdd.path.domain.Station;
+import atdd.path.security.TokenAuthenticationService;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+
+import static atdd.path.TestConstant.STATION_NAME;
+import static atdd.path.fixture.UserFixture.KIM_EMAIL;
+import static atdd.path.web.BaseURlGroup.FAVORITE_BASE_URL;
+
+public class FavoriteAcceptanceTest extends atdd.path.AbstractAcceptanceTest {
+    public static final String FAVORITE_INPUT_JSON = "{\"stationId\":\"1\", \"stationName\":\"강남역\"}";
+
+    private RestWebClientTest restWebClientTest;
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    @BeforeEach
+    void setUp() {
+        cleanAllDatabases();
+        this.restWebClientTest = new RestWebClientTest(this.webTestClient);
+        this.tokenAuthenticationService = new TokenAuthenticationService();
+    }
+
+    @DisplayName("지하철역 즐겨찾기 등록이 성공하는지")
+    @Test
+    public void createFavoriteToStation(SoftAssertions softly) {
+        restWebClientTest.createUser();
+        restWebClientTest.createStation(STATION_NAME);
+
+        //when
+        EntityExchangeResult<Favorite> expectResponse = restWebClientTest.postMethodWithAuthAcceptance
+                (FAVORITE_BASE_URL.getUrl(), FAVORITE_INPUT_JSON, Favorite.class, getJwt());
+
+        //then
+        HttpHeaders responseHeaders = expectResponse.getResponseHeaders();
+        Favorite responseBody = expectResponse.getResponseBody();
+        Station station = responseBody.getStation();
+
+        //then
+        softly.assertThat(responseHeaders.getLocation()).isNotNull();
+        softly.assertThat(station.getName()).isEqualTo(STATION_NAME);
+    }
+
+    private String getJwt() {
+        return tokenAuthenticationService.toJwtByEmail(KIM_EMAIL);
+    }
+}
