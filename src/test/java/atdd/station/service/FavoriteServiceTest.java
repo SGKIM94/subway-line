8741da4240796ff692a084c90ea1d2469917de10
sanggu <tkdrn8578@naver.com>
2020-02-26 23:16:46 +0900

    [MODIFY] Favorite Test 코드 수정

--- src/test/java/atdd/station/service/FavoriteServiceTest.java
+++ src/test/java/atdd/station/service/FavoriteServiceTest.java
@@ -1,9 +1,8 @@
-package atdd.station.service;
+package atdd.path.application;
 
 import atdd.path.SoftAssertionTest;
-import atdd.path.dao.UserDao;
+import atdd.path.dao.FavoriteDao;
 import atdd.path.domain.Favorite;
-import atdd.path.security.TokenAuthenticationService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,8 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static atdd.path.TestConstant.STATION_NAME;
-import static atdd.path.TestConstant.TEST_STATION;
 import static atdd.path.fixture.FavoriteFixture.NEW_FAVORITE;
+import static atdd.path.fixture.FavoriteFixture.NEW_FAVORITE_CREATE_VIEW;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -20,31 +19,22 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 public class FavoriteServiceTest extends SoftAssertionTest {
     private FavoriteService favoriteService;
-    private UserService userService;
-
     @MockBean
-    private UserDao userDao;
-
-    @MockBean
-    private FavoriteService favoriteService;
-
-    @MockBean
-    private TokenAuthenticationService tokenAuthenticationService;
+    private FavoriteDao favoriteDao;
 
     @BeforeEach
     void setUp() {
-        this.tokenAuthenticationService = new TokenAuthenticationService();
-        this.userService = new UserService(tokenAuthenticationService, userDao);
+        this.favoriteService = new FavoriteService(favoriteDao);
     }
 
     @DisplayName("사용자가 지하철 등록에 성공하는지")
     @Test
     public void save() {
         //given
-        when(favoriteService.save(any())).thenReturn(NEW_FAVORITE);
+        when(favoriteDao.save(any())).thenReturn(NEW_FAVORITE);
 
         //when
-        Favorite favorite = favoriteService.save(TEST_STATION);
+        Favorite favorite = favoriteService.save(NEW_FAVORITE_CREATE_VIEW);
 
         //then
         assertThat(favorite.getStation().getName()).isEqualTo(STATION_NAME);
