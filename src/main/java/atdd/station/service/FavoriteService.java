974e5249d610e9039fc5717ac97ae6f574026a27
sanggu <tkdrn8578@naver.com>
2020-02-27 23:57:33 +0900

    [ADD] 즐겨찾기를 저장 및 조회하는 Service layer code 추가

---
+++ src/main/java/atdd/station/service/FavoriteService.java
@@ -0,0 +1,29 @@
+package atdd.station.service;
+
+import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
+import atdd.path.application.dto.favorite.FavoriteListResponseView;
+import atdd.path.dao.FavoriteDao;
+import atdd.path.domain.Favorite;
+import atdd.path.domain.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FavoriteService {
+    private FavoriteDao favoriteDao;
+
+    public FavoriteService(FavoriteDao favoriteDao) {
+        this.favoriteDao = favoriteDao;
+    }
+
+    public Favorite save(User loginUser, FavoriteCreateRequestView favorite) {
+        return favoriteDao.save(favorite.toEntity(loginUser), favorite.getType());
+    }
+
+    public FavoriteListResponseView findByUser(User loginUser) {
+        return FavoriteListResponseView.toDtoEntity(favoriteDao.findByUser(loginUser));
+    }
+
+    public void deleteStation(User loginUser, Long stationId) {
+        favoriteDao.deleteStation(loginUser, stationId);
+    }
+}
