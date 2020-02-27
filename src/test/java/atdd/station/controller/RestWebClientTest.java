3a8bfc2a17fd25b3f1adaaf33b52962d126b7b56
sanggu <tkdrn8578@naver.com>
2020-02-27 23:55:31 +0900

    [ADD] createline, createStation 공통 메서드 추출

--- src/test/java/atdd/station/controller/RestWebClientTest.java
+++ src/test/java/atdd/station/controller/RestWebClientTest.java
@@ -1,14 +1,24 @@
-package atdd.station.controller;
+package atdd.path.web;
 
+import atdd.path.application.dto.station.StationResponseView;
+import atdd.path.domain.Line;
+import atdd.path.domain.User;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
+import static atdd.path.web.LineAcceptanceTest.LINE_INPUT_JSON;
+import static atdd.path.web.LineAcceptanceTest.LINE_URL;
+import static atdd.path.web.StationAcceptanceTest.STATION_URL;
+import static atdd.path.web.UserAcceptanceTest.KIM_INPUT_JSON;
+import static atdd.path.web.UserAcceptanceTest.USER_BASE_URL;
+
 public class RestWebClientTest {
     private static final String NO_AUTHORIZATION = "";
-
     private WebTestClient webTestClient;
 
     public RestWebClientTest(WebTestClient webTestClient) {
@@ -53,5 +63,30 @@ public class RestWebClientTest {
                 .exchange()
                 .expectBody(Void.class)
                 .returnResult();
+    }
+
+    String createUser() {
+        return Objects.requireNonNull(
+                postMethodAcceptance(USER_BASE_URL + "/sigh-up", KIM_INPUT_JSON, User.class)
+                        .getResponseHeaders()
+                        .getLocation()
+                        .getPath());
+    }
+
+    String createStation(String stationName) {
+        String inputJson = "{\"name\":\"" + stationName + "\"}";
+        return Objects.requireNonNull(
+                postMethodAcceptance(STATION_URL, inputJson, StationResponseView.class)
+                        .getResponseHeaders()
+                        .getLocation()
+                        .getPath());
+    }
+
+    String createLine() {
+        return Objects.requireNonNull(
+                postMethodAcceptance(LINE_URL, LINE_INPUT_JSON, Line.class)
+                        .getResponseHeaders()
+                        .getLocation()
+                        .getPath());
     }
 }
