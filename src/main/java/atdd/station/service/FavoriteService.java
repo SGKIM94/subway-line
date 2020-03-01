02a65c6e971894c5c5f400166c80fbf3b9384b87
sanggu <tkdrn8578@naver.com>
2020-03-01 21:08:48 +0900

    [ADD] 즐겨찾기를 등록, 조회, 삭제하는 service 추가

--- src/main/java/atdd/station/service/FavoriteService.java
+++ src/main/java/atdd/station/service/FavoriteService.java
@@ -1,11 +1,16 @@
-package atdd.station.service;
+package atdd.path.application;
 
 import atdd.path.application.dto.favorite.FavoriteCreateRequestView;
+import atdd.path.application.dto.favorite.FavoriteCreateResponseView;
 import atdd.path.application.dto.favorite.FavoriteListResponseView;
 import atdd.path.dao.FavoriteDao;
 import atdd.path.domain.Favorite;
 import atdd.path.domain.User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static atdd.path.dao.FavoriteDao.STATION_TYPE;
 
 @Service
 public class FavoriteService {
@@ -15,15 +20,23 @@ public class FavoriteService {
         this.favoriteDao = favoriteDao;
     }
 
-    public Favorite save(User loginUser, FavoriteCreateRequestView favorite) {
-        return favoriteDao.save(favorite.toEntity(loginUser), favorite.getType());
+    public FavoriteCreateResponseView save(User loginUser, FavoriteCreateRequestView favorite) {
+        return FavoriteCreateResponseView.toDtoEntity(favoriteDao.save(favorite.toEntity(loginUser), favorite.getType()));
     }
 
-    public FavoriteListResponseView findByUser(User loginUser) {
-        return FavoriteListResponseView.toDtoEntity(favoriteDao.findByUser(loginUser));
+    public FavoriteListResponseView findByUser(User loginUser, String type) {
+        return FavoriteListResponseView.toDtoEntity(findStationByUserAndType(loginUser, type));
     }
 
-    public void deleteStation(User loginUser, Long stationId) {
-        favoriteDao.deleteStation(loginUser, stationId);
+    public void deleteItem(User loginUser, Long itemId) {
+        favoriteDao.deleteItem(loginUser, itemId);
+    }
+
+    private List<Favorite> findStationByUserAndType(User loginUser, String type) {
+        if (STATION_TYPE.equals(type)) {
+            return favoriteDao.findStationByUser(loginUser);
+        }
+
+        return favoriteDao.findEdgeByUser(loginUser);
     }
 }
