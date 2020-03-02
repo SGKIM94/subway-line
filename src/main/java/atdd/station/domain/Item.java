5325086fbd87ffccaf475180ec127e526bffc3c8
sanggu <tkdrn8578@naver.com>
2020-03-02 23:23:54 +0900

    [MODIFY] 등록하는 favorite item 에 대한 검증 추가

--- src/main/java/atdd/station/domain/Item.java
+++ src/main/java/atdd/station/domain/Item.java
@@ -1,6 +1,9 @@
-package atdd.station.domain;
+package atdd.path.domain;
 
 import lombok.NoArgsConstructor;
+import org.springframework.dao.DuplicateKeyException;
+
+import static atdd.path.dao.FavoriteDao.EDGE_TYPE;
 
 @NoArgsConstructor
 public class Item {
@@ -8,6 +11,16 @@ public class Item {
 
     public Item(Long id) {
         this.id = id;
+    }
+
+    public void checkSourceAndTargetStationIsSameWhenEdge(String type) {
+        if (!EDGE_TYPE.equals(type)) {
+            return;
+        }
+
+        if (((Edge) this).isSameNameWithSourceAndTarget()) {
+            throw new DuplicateKeyException("시작역과 종착역이 같을 수 없습니다.");
+        }
     }
 
     public Long getId() {
