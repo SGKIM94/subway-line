25a70b9e7108bf50005beedd3cb349daf8087ef9
sanggu <tkdrn8578@naver.com>
2020-03-05 23:46:40 +0900

    [MODIFY] FavoriteCreateRequestView 에 edge 와 station 을 받을 수 있도록 추가

--- src/main/java/atdd/station/dto/favorite/FavoriteCreateRequestView.java
+++ src/main/java/atdd/station/dto/favorite/FavoriteCreateRequestView.java
@@ -1,26 +1,31 @@
 package atdd.path.application.dto.favorite;
 
-import atdd.path.domain.Favorite;
-import atdd.path.domain.Item;
-import atdd.path.domain.User;
-import lombok.Builder;
+import atdd.path.domain.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static atdd.path.dao.FavoriteDao.EDGE_TYPE;
+
 @Getter
-@Builder
 @NoArgsConstructor
 public class FavoriteCreateRequestView {
-    private Item item;
+    private atdd.path.domain.Item item;
+    private atdd.path.domain.Edge edge;
+    private Station station;
     private String type;
 
-    @Builder
-    public FavoriteCreateRequestView(Item item, String type) {
+    public FavoriteCreateRequestView(atdd.path.domain.Item item, String type) {
         this.item = item;
+        if (EDGE_TYPE.equals(type)) {
+            this.edge = (atdd.path.domain.Edge) item;
+        } else {
+            this.station = (Station) item;
+        }
+
         this.type = type;
     }
 
-    public Favorite toEntity(User user) {
+    public Favorite toEntity(atdd.path.domain.User user) {
         return Favorite.builder()
                 .user(user)
                 .item(item)
