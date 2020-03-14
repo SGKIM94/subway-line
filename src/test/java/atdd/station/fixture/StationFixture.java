09923c3a3a63cd9bac1c90d96f5c284bc1c12c40
sanggu <tkdrn8578@naver.com>
2020-03-14 18:16:12 +0900

    [MODIFY] Station 을 추가하는 fixture에 id 값 추가

--- src/test/java/atdd/station/fixture/StationFixture.java
+++ src/test/java/atdd/station/fixture/StationFixture.java
@@ -1,6 +1,7 @@
 package atdd.station.fixture;
 
 import atdd.station.domain.Station;
+import atdd.station.dto.subwayLine.SubwayLineUpdateRequestDto;
 
 import java.util.Arrays;
 import java.util.List;
@@ -8,21 +9,29 @@ import java.util.List;
 import static atdd.station.fixture.SubwaysFixture.SUBWAYS;
 
 public class StationFixture {
+    public static final Long DEFAULT_STATION_ID = 0L;
     public static final String KANGNAM_STATION_NAME = "강남역";
     public static final String YUCKSAM_STATION_NAME = "역삼역";
     public static final String SUNLENG_STATION_NAME = "선릉역";
+    public static final String EMAE_STATION_NAME = "아매역";
     public static final String PANGYO_STATION_NAME = "판교역";
+    public static final String SEHEYN_STATION_NAME = "서현역";
 
-    public static final Station KANGNAM_STATION = new Station(KANGNAM_STATION_NAME);
-    public static final Station YUCKSAM_STATON = new Station(YUCKSAM_STATION_NAME);
-    public static final Station SUNLENG_STATON = new Station(SUNLENG_STATION_NAME);
+    public static final Station KANGNAM_STATION = new Station(0L, KANGNAM_STATION_NAME);
+    public static final Station YUCKSAM_STATON = new Station(1L, YUCKSAM_STATION_NAME);
+    public static final Station SUNLENG_STATON = new Station(2L, SUNLENG_STATION_NAME);
+    public static final Station PANGYO_STATION = new Station(3L, PANGYO_STATION_NAME);
+    public static final Station EMAE_STATION = new Station(4L, EMAE_STATION_NAME);
+    public static final Station SEHYEN_STATION = new Station(5L, SEHEYN_STATION_NAME);
 
     public static final List<Station> KANGNAM_AND_YUCKSAM_STATIONS = Arrays.asList(KANGNAM_STATION, YUCKSAM_STATON);
+    public static final SubwayLineUpdateRequestDto KANGNAM_AND_YUCKSAM_STATIONS_DTO = SubwayLineUpdateRequestDto.toDtoEntity(Arrays.asList(KANGNAM_STATION, YUCKSAM_STATON));
 
     public static final Station PANGYO = new Station(PANGYO_STATION_NAME);
 
     public static Station getStation(String name) {
         return Station.builder()
+                .id(DEFAULT_STATION_ID)
                 .name(name)
                 .subways(SUBWAYS)
                 .build();
