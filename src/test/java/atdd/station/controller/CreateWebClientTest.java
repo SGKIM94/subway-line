94f8c80685b021cc40a8541b4301a5d7b6bd05b7
sanggu <tkdrn8578@naver.com>
2020-03-15 22:45:21 +0900

    [MODIFY] BASE_URL 추출 및 사용하지 않는 User 제거

--- src/test/java/atdd/station/controller/CreateWebClientTest.java
+++ src/test/java/atdd/station/controller/CreateWebClientTest.java
@@ -1,6 +1,5 @@
 package atdd.station.controller;
 
-import atdd.path.domain.User;
 import atdd.station.domain.Edge;
 import atdd.station.domain.Edges;
 import atdd.station.dto.station.StationCreateRequestDto;
@@ -12,34 +11,25 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import java.util.ArrayList;
 import java.util.Objects;
 
-import static atdd.path.web.UserAcceptanceTest.KIM_INPUT_JSON;
-import static atdd.path.web.UserAcceptanceTest.USER_BASE_URL;
-import static atdd.station.docs.UserDocumentationTest.USER_BASE_URL;
 import static atdd.station.fixture.SubwayLineFixture.*;
 
 public class CreateWebClientTest extends atdd.path.web.RestWebClientTest {
+    public static final String LINE_BASE_URL = "/subway-lines/";
+    public static final String STATION_BASE_URL = "/stations/";
 
     public CreateWebClientTest(WebTestClient webTestClient) {
         super(webTestClient);
     }
 
-    String createUser() {
-        return Objects.requireNonNull(
-                postMethodAcceptance(USER_BASE_URL + "/sigh-up", KIM_INPUT_JSON, User.class)
-                .getResponseHeaders()
-                .getLocation()
-                .getPath());
-    }
-
     Long createStation(Long id, String stationName) {
-        StationCreateRequestDto station = new StationCreateRequestDto(id, stationName, new ArrayList<>());
+        StationCreateRequestDto station = new StationCreateRequestDto(id, stationName);
         return Objects.requireNonNull(postMethodAcceptance(STATION_BASE_URL, station, StationCreateResponseDto.class)
                 .getResponseBody()).getId();
     }
 
     Long createLine(String lineName) {
         SubwayLineCreateRequestDto line = new SubwayLineCreateRequestDto
-                (lineName, DEFAULT_START_TIME, DEFAULT_END_TIME, DEFAULT_INTERVAL, new ArrayList<>(), new Edges());
+                (lineName, DEFAULT_START_TIME, DEFAULT_END_TIME, DEFAULT_INTERVAL, new Edges());
 
         return Objects.requireNonNull(postMethodAcceptance(LINE_BASE_URL, line, SubwayLineCreateResponseDto.class)
                 .getResponseBody()).getId();
