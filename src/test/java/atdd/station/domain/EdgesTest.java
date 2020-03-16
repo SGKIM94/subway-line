eef5ccb3cb287a49c50f686540ccb6f49389a3e0
sanggu <tkdrn8578@naver.com>
2020-03-16 23:13:30 +0900

    [ADD] Edge 에 저장되어 있는 모든 Station 을 가져오는지에 대한 테스트 코드 추가

---
+++ src/test/java/atdd/station/domain/EdgesTest.java
@@ -0,0 +1,30 @@
+package atdd.station.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static atdd.station.fixture.StationFixture.*;
+import static atdd.station.fixture.SubwayLineFixture.FIRST_SUBWAY_LINE;
+import static atdd.station.fixture.SubwayLineFixture.SECOND_SUBWAY_LINE;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EdgesTest {
+
+    @DisplayName("Edgs 에 저장되어 있는 모든 Station 을 가져오는지")
+    @Test
+    public void  getStationsByEdgesTest() {
+        //given
+        Edge firstEdge = new Edge(0L, KANGNAM_STATION, PANGYO_STATION, SECOND_SUBWAY_LINE, 10);
+        Edge secondEdge = new Edge(1L, SUNLENG_STATON, EMAE_STATION, FIRST_SUBWAY_LINE, 10);
+        Edges edges = new Edges(Arrays.asList(firstEdge, secondEdge));
+
+        //when
+        List<Station> stations = edges.getStations();
+
+        //then
+        assertThat(stations).hasSize(4);
+    }
+}
