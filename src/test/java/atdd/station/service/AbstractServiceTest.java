e9226340d6ef246b6da079070ed878e599092c99
sanggu <tkdrn8578@naver.com>
2020-03-20 19:45:04 +0900

    [MODIFY] ServiceTest 에 대한 공통 설정을 추출하기 위한 abstractServiceTest 클래스 추가

---
+++ src/test/java/atdd/station/service/AbstractServiceTest.java
@@ -0,0 +1,8 @@
+package atdd.station.service;
+
+import atdd.station.SoftAssertionTest;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AbstractServiceTest extends SoftAssertionTest {
+}
