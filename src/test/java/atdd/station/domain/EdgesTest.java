5e90f338ae20e0fb454f51cab7234f599d9b35c0
sanggu <tkdrn8578@naver.com>
2020-03-21 21:45:17 +0900

    [ADD] Edges 에 edge 를 추가할 수 잇는 test code 추가

--- src/test/java/atdd/station/domain/EdgesTest.java
+++ src/test/java/atdd/station/domain/EdgesTest.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static atdd.station.fixture.StationFixture.*;
-import static atdd.station.fixture.SubwayLineFixture.FIRST_SUBWAY_LINE;
 import static atdd.station.fixture.SubwayLineFixture.SECOND_SUBWAY_LINE;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,16 +27,46 @@ public class EdgesTest {
         assertThat(stations).hasSize(4);
     }
 
-    @DisplayName("강남역 의 이름을 가진 역을 가져오는지")
-    @Test
-    public void  findStationByName() {
-        //given
-        Edge firstEdge = new Edge(0L, KANGNAM_STATION, PANGYO_STATION, SECOND_SUBWAY_LINE, 10);
-        Edge secondEdge = new Edge(1L, SUNLENG_STATON, EMAE_STATION, FIRST_SUBWAY_LINE, 10);
-        Edges edges = new Edges(Arrays.asList(firstEdge, secondEdge));
+    package atdd.station.domain;
 
-        Station station = edges.findStationByName(KANGNAM_STATION_NAME);
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-        assertThat(station.getName()).isEqualTo(KANGNAM_STATION_NAME);
+import java.util.Arrays;
+import java.util.List;
+
+import static atdd.station.fixture.StationFixture.*;
+import static atdd.station.fixture.SubwayLineFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+    public class EdgesTest {
+
+        @DisplayName("Edgs 에 저장되어 있는 모든 Station 을 가져오는지")
+        @Test
+        public void  getStationsByEdgesTest() {
+            //when
+            List<Station> stations = FIRST_EDGES.getStations();
+
+            //then
+            assertThat(stations).hasSize(4);
+        }
+
+        @DisplayName("강남역 의 이름을 가진 역을 가져오는지")
+        @Test
+        public void  findStationByName() {
+            Station station = FIRST_EDGES.findStationByName(KANGNAM_STATION_NAME);
+
+            assertThat(station.getName()).isEqualTo(KANGNAM_STATION_NAME);
+        }
+
+        @DisplayName("노선의 구간들에 새로운 구간을 추가할 수 있는지")
+        @Test
+        public void addEdge() {
+            //give
+            Edges addedEdges = FIRST_EDGES.addEdge(THIRD_EDGE);
+
+            assertThat(addedEdges.getSize()).isEqualTo(3);
+        }
     }
+
 }
