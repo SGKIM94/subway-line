a07bd9aebd4859d71d0dbcfd48a43a88eda99960
sanggu <tkdrn8578@naver.com>
2020-03-28 17:45:25 +0900

    [ADD] Spring rest docs 에서 request, response 의 snippet 의 중복을 제거하기 위한 FieldsSnippet 클래스 추가

---
+++ src/test/java/atdd/station/docs/FieldsSnippet.java
@@ -0,0 +1,41 @@
+package atdd.station.docs;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.restdocs.headers.RequestHeadersSnippet;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
+import org.springframework.restdocs.payload.ResponseFieldsSnippet;
+
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+
+public class FieldsSnippet {
+    private RestDocumentationResultHandler writeResultDocument(String identifier
+            , RequestFieldsSnippet requestFieldsSnippet, ResponseFieldsSnippet responseFieldsSnippet) {
+        return document(identifier, requestFieldsSnippet, responseFieldsSnippet);
+    }
+
+    RestDocumentationResultHandler writeResultDocument(String identifier , RequestFieldsSnippet requestFieldsSnippet) {
+        return document(identifier, requestFieldsSnippet);
+    }
+
+    private FieldDescriptor writeFieldSnippetDescription(String keyName, JsonFieldType type, String description) {
+        return fieldWithPath(keyName).type(type).description(description);
+    }
+
+    FieldDescriptor writeStringSnippetDescription(String key, String description) {
+        return fieldWithPath(key).type(JsonFieldType.STRING).description(description);
+    }
+
+    FieldDescriptor writeNumberSnippetDescription(String key, String description) {
+        return fieldWithPath(key).type(JsonFieldType.NUMBER).description(description);
+    }
+
+    RequestHeadersSnippet getAuthorizationHeaderSnippet(String description) {
+        return requestHeaders(headerWithName(HttpHeaders.AUTHORIZATION).description(description));
+    }
+}
