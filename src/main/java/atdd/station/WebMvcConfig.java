a3e0616190cb1b80a4a26a259ee9a3b431d30ef3
sanggu <tkdrn8578@naver.com>
2020-04-08 23:05:02 +0900

    [MODIFY] LoginUser annotation 을 argument 로 사용하기 위한 addArgumentResolvers 추가 및 jwtAuthInterceptor 를 Bean 으로 등록

--- src/main/java/atdd/station/WebMvcConfig.java
+++ src/main/java/atdd/station/WebMvcConfig.java
@@ -1,12 +1,51 @@
 package atdd.station;
 
+import atdd.station.domain.UserRepository;
+import atdd.station.security.JwtAuthInterceptor;
+import atdd.station.security.TokenAuthenticationService;
+import atdd.station.security.UserHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private UserRepository userRepository;
+
+    private static String[] INTERCEPTOR_WITHE_LIST = {
+            "/users/login",
+            "/users/sigh-up",
+            "/stations/**",
+            "/paths/**",
+            "/lines/**",
+    };
+
+    public WebMvcConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(jwtAuthInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns(INTERCEPTOR_WITHE_LIST);
+    }
+
+    @Bean
+    public JwtAuthInterceptor jwtAuthInterceptor() {
+        return new JwtAuthInterceptor(new TokenAuthenticationService(), userRepository);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserHandlerMethodArgumentResolver());
+    }
+
     @Bean
     public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
         return new ShallowEtagHeaderFilter();
