9173a3d0b34748044944b5d50c0af16b47f1b192
sanggu <tkdrn8578@naver.com>
2020-04-09 23:53:46 +0900

    [MODIFY] Token interceptor 에서 user API 외에는 검사하도록 변경

--- src/main/java/atdd/station/WebMvcConfig.java
+++ src/main/java/atdd/station/WebMvcConfig.java
@@ -20,9 +20,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     private static String[] INTERCEPTOR_WITHE_LIST = {
             "/users/login",
             "/users/sigh-up",
-            "/stations/**",
-            "/paths/**",
-            "/lines/**",
     };
 
     public WebMvcConfig(UserRepository userRepository) {
